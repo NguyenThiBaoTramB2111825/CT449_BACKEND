e9c06557f911f7883e56ebd07ef63629453c54cf
NguyenThiBaoTramB2111825 <tramB2111825@student.ctu.edu.vn>
2024-09-19 19:15:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
